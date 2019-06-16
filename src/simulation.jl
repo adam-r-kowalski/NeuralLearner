1763f06c494dd1552944d55f743ee6a02b82b598
Adam Kowalski <adam.kowalski.work@gmail.com>
2019-06-16 15:37:17 -0700

    remove constructor for simulation

--- src/simulation.jl
+++ src/simulation.jl
@@ -1,23 +1,20 @@
-struct Simulation{Environment, Agent}
+struct Simulation{
+        ObservationSpace <: AbstractSpace,
+        ActionSpace <: AbstractSpace,
+        Observation,
+        Action,
+        Environment <: AbstractEnvironment{ObservationSpace, ActionSpace},
+        Agent <: AbstractAgent{Observation, Action, ObservationSpace, ActionSpace},
+        }
     env::Environment
     agent::Agent
-    render::Bool
-
-    Simulation(env::AbstractEnvironment{ObservationSpace, ActionSpace},
-               agent::AbstractAgent{Observation, Action, ObservationSpace, ActionSpace},
-               render::Bool=false
-               ) where {ObservationSpace<:AbstractSpace,
-                        ActionSpace<:AbstractSpace,
-                        Observation,
-                        Action} =
-        new{typeof(env), typeof(agent)}(env, agent, render)
 end
 
 
 Base.IteratorSize(::Simulation) = Base.IsInfinite()
 
 
-Base.eltype(::Simulation{Environment{Box{T, N}, Box{T2, N2}}}
+Base.eltype(::Simulation{Box{T, N}, Box{T2, N2}}
            ) where {T, N, T2, N2} =
     Transition{Array{T, N}, Array{T2, N2}}
 
@@ -25,7 +22,6 @@ Base.eltype(::Simulation{Environment{Box{T, N}, Box{T2, N2}}}
 function Base.iterate(sim::Simulation, observation=reset(sim.env))
     action = select_action(sim.agent, observation)
     next_observation, reward, done = step(sim.env, action)
-    sim.render && render(sim.environment)
     transition = Transition(observation, action, reward, next_observation, done)
     remember!(sim.agent, transition)
     transition, done ? reset(sim.env) : next_observation
